3188303055326d377da0e86e7ce7bc476ffafa21
fullkon88 <fullkon999@gmail.com>
2023-10-09 11:15:57 +0300

    сделали таск12

--- task12/Program.cs
+++ task12/Program.cs
@@ -4,5 +4,17 @@
 34, 5 -> некратно, остаток 4
 16, 4 -> кратно */
 
+System.Console.WriteLine("Введи два числа: ");
 
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2 * number2)
+{
+System.Console.WriteLine("Да");    
+}
+else 
+{
+    System.Console.WriteLine("Числа некратны, остаток от деления " + number1 % number2);
+}
 
