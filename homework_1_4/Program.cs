92518995695950f4f2451d1014158e2a07156fca
fullkon88 <fullkon999@gmail.com>
2023-10-12 02:53:59 +0300

    Сделал дз

---
+++ homework_1_4/Program.cs
@@ -0,0 +1,12 @@
+﻿/* PrintEvenNumbers, которая на вход принимает число (number), 
+а на выходе выводит все чётные числа от 1 до number (включительно),
+ разделеные знаком табуляции.
+*/
+Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+for(int count = 2; count <= number1; count += 2)
+{
+            Console.Write($"Четное число: {count}\t");
+}
+
