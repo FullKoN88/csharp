f4ee4ee30dc91e9e111030144dca12c19baeaec5
fullkon88 <fullkon999@gmail.com>
2023-10-12 02:30:58 +0300

    Сделал дз 1,2

---
+++ homework_1_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите метод FindMax, который принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2)
+
+max = number2;
+
+if (max < number3)
+
+max = number3;
+{
+    Console.WriteLine("Максимальное число: ");
+}
+int result = max;
+System.Console.WriteLine(result);
+
