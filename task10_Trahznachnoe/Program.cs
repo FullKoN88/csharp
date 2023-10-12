39ff81a7ad2d1b31b588a51259c403680472c916
fullkon88 <fullkon999@gmail.com>
2023-10-12 03:05:31 +0300

    Сделал дз

---
+++ task10_Trahznachnoe/Program.cs
@@ -0,0 +1,11 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+System.Console.WriteLine("Введите число: ");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+int result = userNumber1 / 10 % 10;
+System.Console.WriteLine(result);
