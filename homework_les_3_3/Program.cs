57a5e58a71d2cec3c1f77942d06537630f1803a3
fullkon88 <fullkon999@gmail.com>
2023-10-16 10:03:16 +0300

    Сделано дз 3

---
+++ homework_les_3_3/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+напишите метод ShowCube, который принимает на вход число (N) и 
+выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+N = 3 
+1
+8
+27
+
+N = 5
+1
+8
+27
+64
+125
+*/
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int userNumber = int.Parse(Console.ReadLine());
+Console.Clear();
+for (int i = 1; i <= userNumber; i++)
+{
+    System.Console.WriteLine($"{i} * {i} = {Math.Pow(i, 2)}");
+}
